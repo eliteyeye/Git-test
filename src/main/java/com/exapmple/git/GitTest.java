d75fee662917ec3d0084cacc135178b0a87bc278
小赵 <2474424702@qq.com>
2022-12-13 16:15:25 +0800

    第一次在idea中提交

---
+++ src/main/java/com/exapmple/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.exapmple.git;
+
+/**
+ * @author $
+ * @version 1.0.0
+ * @description: TODO
+ * @date $ $
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test 111111");
+    }
+}
