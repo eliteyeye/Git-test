9a5d8716f218df5c134e8a5abd0bee305d084b8f
小赵 <2474424702@qq.com>
2022-12-13 16:15:42 +0800

    第二次在idea中提交

--- src/main/java/com/exapmple/git/GitTest.java
+++ src/main/java/com/exapmple/git/GitTest.java
@@ -9,5 +9,6 @@ package com.exapmple.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test 111111");
+        System.out.println("git test 2222222222");
     }
 }
