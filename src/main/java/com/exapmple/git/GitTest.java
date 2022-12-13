1528aabb886b8ed0ced131ec6765521757e21c47
小赵 <2474424702@qq.com>
2022-12-13 19:43:10 +0800

    hot-fix 在idea中提交

--- src/main/java/com/exapmple/git/GitTest.java
+++ src/main/java/com/exapmple/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test 111111");
         System.out.println("git test 2222222222");
+        System.out.println("git test 3333333333");
     }
 }
